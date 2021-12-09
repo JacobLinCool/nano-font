7d425ea7b419657d95c709c0039c6adc76253384
JacobLinCool <jacoblincool@gmail.com>
2021-12-10 00:58:19 +0800

    feat(gen): add force_rebuild option

--- src/build.ts
+++ src/build.ts
@@ -9,7 +9,8 @@ dotenv.config();
 
 build();
 
-async function build() {
+async function build(force_rebuild = false) {
+    const START_TIME = Date.now();
     if (!process.env.GOOGLE_API_KEY) {
         throw new Error("GOOGLE_API_KEY is not defined. Run npm run gen again after setting it.");
     }
@@ -28,23 +29,27 @@ async function build() {
     // @ts-ignore
     await woff2.init();
 
-    let index_ts = "";
     const font_list: string[] = [];
 
-    for (let i = 0; i < google_font_list.length; i++) {
+    const list_length = google_font_list.length;
+    const padding = Math.log10(list_length) + 1;
+
+    for (let i = 0; i < list_length; i++) {
         const font = google_font_list[i];
+        const font_name = font.family.replace(/ /g, "_");
+        const font_path = path.resolve(__dirname, "fonts", `${font_name}.ts`);
+
         if (font.files.regular) {
             try {
-                process.stdout.write(
-                    `[${(i + 1).toString().padStart(Math.log10(google_font_list.length) + 1)} / ${google_font_list.length}] ${font.family}... `,
-                );
-                const base64 = await get_font_base64(font.files.regular);
-                fs.writeFileSync(
-                    path.resolve(__dirname, "fonts", `${font.family.replace(/ /g, "_")}.ts`),
-                    `export const name = "${font.family}"; export const base64 = "${base64}";`,
-                );
-                index_ts += `export * as ${font.family.replace(/ /g, "_")} from "./${font.family.replace(/ /g, "_")}";\n`;
-                font_list.push(font.family.replace(/ /g, "_"));
+                process.stdout.write(`[${(i + 1).toString().padStart(padding)} / ${list_length}] ${font.family}... `);
+
+                if (force_rebuild || !fs.existsSync(font_path)) {
+                    const base64 = await get_font_base64(font.files.regular);
+                    fs.writeFileSync(font_path, `export const name = "${font.family}"; export const base64 = "${base64}";`);
+                }
+
+                font_list.push(font_name);
+
                 console.log("\u001b[92mDONE\u001b[m");
             } catch (err) {
                 console.log("\u001b[91mFAILED\u001b[m");
@@ -52,8 +57,10 @@ async function build() {
         }
     }
 
-    fs.writeFileSync(path.resolve(__dirname, "fonts", "index.ts"), index_ts);
+    fs.writeFileSync(path.resolve(__dirname, "fonts", "index.ts"), font_list.map((font_name) => `export * as ${font_name} from "./${font_name}";`).join("\n"));
     fs.writeFileSync(path.resolve(__dirname, "fonts", "types.ts"), `export type FontFamily = ${font_list.map((font) => `"${font}"`).join(" | ")};`);
+
+    console.log(`\n\u001b[92mFont generating completed in ${(Date.now() - START_TIME) / 1000}s\u001b[m`);
 }
 
 async function get_font_base64(url: string) {
@@ -68,3 +75,5 @@ async function get_font_base64(url: string) {
 
     return font.toBase64({ type: "woff2" }, font.write({ type: "woff2", hinting: true }));
 }
+
+export default build;
